1e1bced4924af82cb8271708e416c390db2da3b6
janirefdez <janire_fernandez94@hotmail.com>
2022-02-20 17:34:48 +0100

    Initial Commit

---
+++ WeatherApp/WeatherAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherAppApp.swift
+//  WeatherApp
+//
+//  Created by Janire Fernandez on 20/2/22.
+//
+
+import SwiftUI
+
+@main
+struct WeatherAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
